fe1407556249d1630d3d22cb10a7b7458c543a3a
Anna <anna1995dzhym@gmail.com>
2018-11-13 13:27:06 +0200

    done with changed design

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@
 
 (function ($) {
     var timer = 700;
-    $('.acordion__element--title').click(function () {
+
+    //Two variants for accordion
+    //Variant 1
+
+    // $('.accordion__element--title').on('click', showInfo);
+    //
+    // function showInfo(){
+    //     $('.info').not($(this).next()).slideUp(timer).parent();
+    //     $(this).next().slideToggle(timer);
+    // };
+
+    //Variant 2
+
+    $('.accordion__element--title').click(function () {
         var $this = $(this);
         var $parent = $this.parent();
 
@@ -20,17 +33,25 @@
     });
 
     $('.clients__button').click(open);
-    $('.close').click(close);
-    $('.buttons').click(cancel);
+    $('.popup__close').click(close);
+    $('.popup__buttons').click(cancel);
     function open(){
-        $('.popup').fadeIn('fast');
-        $('.popup__title')
+        $('.popup').fadeIn(timer);
+        $('.popup__title').text(function () {
+           var $title = '';
+           if ($('.accordion__element').hasClass('show')) {
+               $title = $('.accordion__element.show').children('.accordion__element--title').text()
+           } else {
+               $title = 'Accordion is not open';
+           }
+           return $title;
+        });
     };
     function close() {
-        $('.popup').fadeOut('slow');
+        $('.popup').fadeOut(timer);
     }
     function cancel() {
-        $('.popup').fadeOut('slow');
+        $('.popup').fadeOut(timer);
         console.log('You chose CANCEL or Ok.');
     }
 })(jQuery);
